9ef3595f4ae452571a6db56e365d5a659654d1ad
Seth Rudesill <sethrudesill@gmail.com>
2017-12-26 13:55:54 -0500

    Added an app that doesn't do anything except make certain it can modify settings on System.Console.

---
+++ src/MasterMUD.App/App.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace MasterMUD
+{
+    public sealed partial class App
+    {
+        /// <summary>
+        ///     Initializes the environment and performs all required work before Main is allowed to run.
+        /// </summary>
+        /// <remarks>If anything goes wrong in here, we need to do our very best to shutdown the running application.</remarks>
+        static App()
+        {
+            if (System.Environment.UserInteractive)
+                try
+                {
+                    // TODO: Environment configuration and initialization.
+                    System.Console.Title = Properties.Resources.Title;
+                    System.Console.Clear();
+                    System.Console.CursorVisible = false;
+                }
+                catch (System.Exception ex)
+                {
+                    try
+                    {
+                        System.Console.Error.WriteLine(ex);
+                    }
+                    catch (System.Exception ex2)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex2);
+                    }
+                    finally
+                    {
+                        var failedToExit = false;
+
+                        try
+                        {
+                            System.Environment.Exit(ex.HResult);
+                        }
+                        catch (System.Security.SecurityException se)
+                        {
+                            failedToExit = true;
+                            System.Diagnostics.Debug.WriteLine(se);
+                        }
+                        finally
+                        {
+                            if (failedToExit)
+                            {
+                                // TODO: What do if couldn't exit?
+                                throw new App.Exception(Properties.Resources.StaticConstructorCatastrophic);
+                            }
+                        }
+                    }
+                }
+        }
+
+        private static void Main() => new App.Exception("Main Not Yet Implemented.", new NotImplementedException(nameof(Main)));
+    }
+}
---
+++ src/MasterMUD.App/Exceptions/App.Exceptions.cs
@@ -0,0 +1,23 @@
+﻿namespace MasterMUD
+{
+    public sealed partial class App
+    {
+        internal sealed class Exception : System.Exception
+        {
+            protected internal Exception() : base()
+            {
+
+            }
+
+            protected internal Exception(string message) : base(message)
+            {
+
+            }
+
+            protected internal Exception(string message, System.Exception innerException) : base(message, innerException)
+            {
+
+            }
+        }
+    }
+}
